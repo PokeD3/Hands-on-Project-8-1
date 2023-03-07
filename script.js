d864b552fdf8b7b14c2b6f04537558158bf0f0c5
PokeD3 <122342237+PokeD3@users.noreply.github.com>
2023-03-06 18:55:23 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+/*
+    JavaScript 6th Edition
+    Chapter 8
+    Hands-on Project 8-1
+
+    Author: Douglas Faulkner
+    Date:   March 5, 2023
+
+    Filename: index.htm
+*/
+//Step4
+"use strict";
+var newAccountArray = new Array();
+//Step5
+function createID() {
+    var fname= document.getElementById("fnameinput"); 
+    var lname= document.getElementById("lnameinput");
+    var zip= document.getElementById("zipinput");
+    var account= document.getElementById("accountidbox");
+    var fields= document.getElementsByTagName("input");
+    var acctid;
+    var firstInit;
+    var lastInit;
+    //Step6
+    if (fname != "") {firstInit = fname.value.charAt(0).toUpperCase();}
+    if (lname != "") {lastInit = lname.value.charAt(0).toUpperCase();}
+	if (acctid!= "") {acctid = firstInit + lastInit + zip.value;}
+    account.value = acctid; 
+    newAccountArray = [];
+    for (var i = 0; i < fields.length - 1; i++) {
+        newAccountArray.push(fields[i].value);
+    }
+}
+//Step7
+function createEventListeners() {
+    var fname = document.getElementById("fnameinput");
+    var lname = document.getElementById("lnameinput");
+    var zip = document.getElementById("zipinput");
+	if(fname.addEventListener) {
+        fname.addEventListener("change", createID, false);
+        lname.addEventListener("change", createID, false);
+        zip.addEventListener("change", createID, false);
+    } else if (fname.attachEvent) {
+        fname.attachEvent("onchange", createID);
+        lname.attachEvent("onchange", createID);
+        zip.attachEvent("onchange", createID);
+    }
+}
+if (window.addEventListener) {
+    window.addEventListener ("load", createEventListeners, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", createEventListeners);
+}
